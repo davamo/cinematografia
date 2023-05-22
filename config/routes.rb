48a414450eba15f1903a733dcbb8b28cf920f7a4
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-22 19:37:06 -0400

    Feat: routes *.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   resources :documentary_films, only: [:index, :new, :create]
-
-  get 'documentary_films/index'
-  get 'documentary_films/create'
-  get 'documentary_films/new'
-
-  get 'series/index'
-  get 'series/create'
-  get 'series/new'
-
-  get 'movies/index'
-  get 'movies/create'
-  get 'movies/new'
-  
-  # Otras rutas de tu aplicación...
-
-  root to: "home#index"
-
+  resources :movies, only: [:index, :new, :create]
+  resources :series, only: [:index, :new, :create]
+  root to: "series#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# db/seeds.rb
+
+# Crear películas
+Movie.create(name: "Back to the Future", synopsis: "viajes en el tiempo", director: "nose")
+Movie.create(name: "Back to the Future II", synopsis: "Sinopsis de la Película 2", director: "Director")
+Movie.create(name: "Back to the Future II", synopsis: "Sinopsis de la Película 2", director: "Director")
+Movie.create(name: "Matrix", synopsis: "Mundo destruido y consquistado por las maquinas", director: "Director")
+# Crear series
+Serie.create(name: "Smallville", synopsis: "Sinopsis de la Serie 1", director: "Director 1")
+Serie.create(name: "Lois and Clark", synopsis: "Sinopsis de la Serie 2", director: "Director 2")
+Serie.create(name: "Toulsa", synopsis: "Sinopsis de la Serie 2", director: "Director 2")
+Serie.create(name: "Sillicon Valley", synopsis: "Sinopsis de la Serie 2", director: "Director 2")
+# Crear documentales
+DocumentaryFilm.create(name: "Hackers", synopsis: "Sinopsis del Documental ", director: "Director 1")
+DocumentaryFilm.create(name: "games ...", synopsis: "Sinopsis del Documental ", director: "Director 2")
+DocumentaryFilm.create(name: "Toulsa", synopsis: "Sinopsis del Documental ", director: "Director 2")
+DocumentaryFilm.create(name: "La Tierra", synopsis: "Sinopsis del Documental ", director: "Director 2")
